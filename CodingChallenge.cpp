2352ea1c381857b9997e7e5aae46b0efe9a970a2
djsmith951 <djsmith951@gmail.com>
2017-04-11 21:21:20 -0500

    moved some function up(no good)

--- CodingChallenge.cpp
+++ CodingChallenge.cpp
@@ -242,6 +242,11 @@ public:
 																						}
 																						//=====6 Condition Cases======
 																						else{
+																							if (board[4] == 'X' && board[5] == 'X' && board[8] == 'X' && board[2] == 'O' && board[3] == 'O' && board[0] != 'X'){
+																								board[0] = 'O';
+																								foundSpot = true;
+																							}
+																						else{
 																							if (board[4] == 'X' && board[5] == 'X' && board[1] == 'X'&& board[2] == 'O' && board[3] == 'O'){
 																								board[7] = 'O';
 																								foundSpot = true;
@@ -360,11 +365,7 @@ public:
 																																												foundSpot = true;
 																																											}
 
-																																											else{
-																																												if (board[4] == 'X' && board[5] == 'X' && board[8] == 'X' && board[2] == 'O' && board[3] == 'O' && board[0] != 'X'){
-																																													board[0] = 'O';
-																																													foundSpot = true;
-																																												}
+																																											
 																																												else{
 																																													if (board[4] == 'O' && board[8] == 'O' && board[6] == 'X' && board[7] == 'X' && board[0] == 'X' && board[3] != 'X'){
 																																														board[3] = 'O';
